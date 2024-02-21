04922fc4a039fd4203dac137f07d980c1f9365b2
Leo <johndoe@example.com>
2024-02-21 11:02:08 +0100

    added functions for theme switching

--- themeswitcher.js
+++ themeswitcher.js
@@ -16,10 +16,33 @@ function switchTheme()
 
     if(path === darkmode)
     {
-        document.getElementById('stylesheet').href = 'main_light.css'
+        setLightmode()
     }
     else if(path === lightmode)
     {
-        document.getElementById('stylesheet').href = 'main.css'
+        setDarkmode()
     }
+}
+
+function setTheme(themeIndex)
+{
+    let index = themeIndex
+
+    switch(index)
+    {
+        case 1:
+            applyTheme('main.css')
+            break;
+        case 2:
+            applyTheme('main_light.css')
+            break;
+        case 3:
+            console.log("wip")
+            break;
+    }
+}
+
+function applyTheme(themeCSS)
+{
+    document.getElementById('stylesheet').href = themeCSS
 }
