be9ecbb922e8fb061a76bfc25105dfaf848b9b45
Leo W. <leowmr1308@gmail.com>
2024-06-14 14:26:51 +0200

    fundamentally restructured code

--- calculate.js
+++ calculate.js
@@ -1,91 +1,91 @@
-function calculate() {
-    let inputDec = document.getElementById('dec')
-    let inputBin = document.getElementById('bin')
-    let inputHex = document.getElementById('hex')
+const inputDec = document.getElementById("dec");
+const inputBin = document.getElementById("bin");
+const inputHex = document.getElementById("hex");
 
-    if(inputDec.value === 0 && inputHex.value === 0 && inputBin.value === 0) {
-        clearInputs()
-    } else if(inputDec.value === 0 && inputHex.value === 0) {
-        calcWhenBinInput()
-    } else if(inputBin.value === 0 && inputHex.value === 0) {
-        calcWhenDecInput()
-    } else if(inputBin.value === 0 && inputDec.value === 0) {
-        calcWhenHexInput()
-    } else {
-        calcWhenDecInput()
-    }
+function calculate() {
+  if (inputDec.value === 0 && inputHex.value === 0 && inputBin.value === 0) {
+    clearInputs();
+  } else if (inputDec.value === 0 && inputHex.value === 0) {
+    calcWhenBinInput();
+  } else if (inputBin.value === 0 && inputHex.value === 0) {
+    calcWhenDecInput();
+  } else if (inputBin.value === 0 && inputDec.value === 0) {
+    calcWhenHexInput();
+  } else {
+    calcWhenDecInput();
+  }
 }
 
 function calcWhenBinInput() {
-    let input = document.getElementById('bin').value
-    let dec = parseInt(input, 2)
-    let hex = parseInt(input, 2).toString(16).toUpperCase()
+  const input = inputBin.value;
+  const dec = parseInt(input, 2);
+  const hex = parseInt(input, 2).toString(16).toUpperCase();
 
-    document.getElementById('dec').value = dec
-    setHexValue(hex)
+  inputDec.value = dec;
+  setHexValue(hex);
 }
 
 function calcWhenDecInput() {
-    let input = document.getElementById('dec').value
-    let bin = parseInt(input, 10).toString(2)
-    let hex = parseInt(input, 10).toString(16).toUpperCase()
+  const input = inputDec.value;
+  const bin = parseInt(input, 10).toString(2);
+  const hex = parseInt(input, 10).toString(16).toUpperCase();
 
-    document.getElementById('bin').value = bin
-    setHexValue(hex)
+  inputBin.value = bin;
+  setHexValue(hex);
 }
 
 function calcWhenHexInput() {
-    let input = document.getElementById('hex').value
-    let dec = parseInt(input, 16)
-    let bin = parseInt(dec, 10).toString(2)
+  const input = document.getElementById("hex").value;
+  const dec = parseInt(input, 16);
+  const bin = parseInt(dec, 10).toString(2);
 
-    document.getElementById('dec').value = dec
-    document.getElementById('bin').value = bin
+  inputDec.value = dec;
+  inputBin.value = bin;
 }
 
 function clearInputs() {
-    document.getElementById('dec').value = ''
-    document.getElementById('bin').value = ''
-    document.getElementById('hex').value = ''
+  inputDec.value = "";
+  inputBin.value = "";
+  inputHex.value = "";
 }
 
 function setHexValue(hex) {
-    if(!(hex === "NAN")) {
-        document.getElementById('hex').value = hex
-    } else {
-        document.getElementById('hex').value = ''
-    }
+  if (!(hex === "NAN")) {
+    inputHex.value = hex;
+  } else {
+    inputHex.value = "";
+  }
 }
 
 function filterHex(event) {
-    const key = event.key
-    const validCharacters = "abcdefABCDEF0123456789"
-    const isNotBackspace = keyIsNotBackSpace(key)
-    if(!validCharacters.includes(key) && isNotBackspace) {
-        event.preventDefault()
-    }
+  const key = event.key;
+  const validCharacters = "abcdefABCDEF0123456789";
+  const isNotBackspace = keyIsNotBackSpace(key);
+  if (!validCharacters.includes(key) && isNotBackspace) {
+    event.preventDefault();
+  }
 }
 
 function filterBin(event) {
-    const key = event.key
-    const validCharacters = "10"
-    const isNotBackspace = keyIsNotBackSpace(key)
-    if(!validCharacters.includes(key) && isNotBackspace) {
-        event.preventDefault()
-    }
+  const key = event.key;
+  const validCharacters = "10";
+  const isNotBackspace = keyIsNotBackSpace(key);
+  if (!validCharacters.includes(key) && isNotBackspace) {
+    event.preventDefault();
+  }
 }
 
 function filterDec(event) {
-    const key = event.key
-    const validCharacters = "0123456789"
-    const isNotBackspace = keyIsNotBackSpace(key)
-    if(!validCharacters.includes(key) && isNotBackspace) {
-        event.preventDefault()
-    }
+  const key = event.key;
+  const validCharacters = "0123456789";
+  const isNotBackspace = keyIsNotBackSpace(key);
+  if (!validCharacters.includes(key) && isNotBackspace) {
+    event.preventDefault();
+  }
 }
 
 function keyIsNotBackSpace(key) {
-    let result = key === "Backspace"
+  const result = key === "Backspace";
 
-    return !(result)
+  return !result;
 }
