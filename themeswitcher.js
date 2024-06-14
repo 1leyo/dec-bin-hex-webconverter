dc2a45ede59c52e2436d243e10713ef1c01173ad
Leo W. <leowmr1308@gmail.com>
2024-06-14 14:38:10 +0200

    improvement: code structure

--- themeswitcher.js
+++ themeswitcher.js
@@ -1,50 +1,41 @@
-let style = document.getElementById('stylesheet');
+const style = document.getElementById("stylesheet");
 
-function setDarkmode()
-{
-    style.href = 'main.css'
+function setDarkmode() {
+  style.href = "main.css";
 }
 
-function setLightmode()
-{
-    style.href = 'main_light.css'
+function setLightmode() {
+  style.href = "main_light.css";
 }
 
-function switchTheme()
-{
-    let darkmode = 'main.css'
-    let lightmode = 'main_light.css'
-    let path = style.getAttribute('href')
+function switchTheme() {
+  const darkmode = "main.css";
+  const lightmode = "main_light.css";
+  const path = style.getAttribute("href");
 
-    if(path === darkmode)
-    {
-        setLightmode()
-    }
-    else if(path === lightmode)
-    {
-        setDarkmode()
-    }
+  if (path === darkmode) {
+    setLightmode();
+  } else if (path === lightmode) {
+    setDarkmode();
+  }
 }
 
-function setTheme(themeIndex)
-{
-    let index = themeIndex
+function setTheme(themeIndex) {
+  let index = themeIndex;
 
-    switch(index)
-    {
-        case 1:
-            applyTheme('main.css')
-            break;
-        case 2:
-            applyTheme('main_light.css')
-            break;
-        case 3:
-            applyTheme('neon_theme.css')
-            break;
-    }
+  switch (index) {
+    case 1:
+      applyTheme("main.css");
+      break;
+    case 2:
+      applyTheme("main_light.css");
+      break;
+    case 3:
+      applyTheme("neon_theme.css");
+      break;
+  }
 }
 
-function applyTheme(themeCSS)
-{
-    style.href = themeCSS
+function applyTheme(themeCSS) {
+  style.href = themeCSS;
 }
