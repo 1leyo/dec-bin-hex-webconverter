28e92b0541f7cf61981e2b4381c2ff58a3c49a40
Leo W. <leowmr1308@gmail.com>
2024-07-27 19:22:28 +0200

    rework: theme switching system now accessed using eventlisteners instead of being part of the HTML. JS completely reworked and way more efficient.

--- javascript/themeswitcher.js
+++ javascript/themeswitcher.js
@@ -1,41 +1,15 @@
 const style = document.getElementById("stylesheet");
 
-function setDarkmode() {
-  style.href = "main.css";
-}
+const option1 = document.getElementById("theme1");
+const option2 = document.getElementById("theme2");
+const option3 = document.getElementById("theme3");
 
-function setLightmode() {
-  style.href = "main_light.css";
-}
-
-function switchTheme() {
-  const darkmode = "main.css";
-  const lightmode = "main_light.css";
-  const path = style.getAttribute("href");
-
-  if (path === darkmode) {
-    setLightmode();
-  } else if (path === lightmode) {
-    setDarkmode();
-  }
-}
-
-function setTheme(themeIndex) {
-  let index = themeIndex;
-
-  switch (index) {
-    case 1:
-      applyTheme("main.css");
-      break;
-    case 2:
-      applyTheme("main_light.css");
-      break;
-    case 3:
-      applyTheme("neon_theme.css");
-      break;
-  }
-}
+const themes = ["main.css", "main_light.css", "neon_theme.css"];
 
 function applyTheme(themeCSS) {
   style.href = `stylesheets/${themeCSS}`;
 }
+
+option1.addEventListener("click", () => applyTheme(themes[0]));
+option2.addEventListener("click", () => applyTheme(themes[1]));
+option3.addEventListener("click", () => applyTheme(themes[2]));
