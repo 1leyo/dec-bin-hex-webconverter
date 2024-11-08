98443bd50a5d9203e3c466a47d1a42d109287df6
Leo W. <leowmr1308@gmail.com>
2024-11-08 20:09:07 +0100

    refactor: update theme switching to use event listeners and local storage

--- javascript/themeswitcher.js
+++ javascript/themeswitcher.js
@@ -1,15 +1,22 @@
 const style = document.getElementById("stylesheet");
-
-const option1 = document.getElementById("theme1");
-const option2 = document.getElementById("theme2");
-const option3 = document.getElementById("theme3");
-
+const themeButtons = document.querySelectorAll(".dropdown-content a");
 const themes = ["main.css", "main_light.css", "neon_theme.css"];
+
+// Aktuelles Theme aus dem Local Storage laden
+const savedTheme = localStorage.getItem("selectedTheme");
+if (savedTheme) {
+  console.log("Loaded Theme from Local Storage");
+  applyTheme(savedTheme);
+}
+
+themeButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const selectedTheme = themes[index];
+    applyTheme(selectedTheme);
+    localStorage.setItem("selectedTheme", selectedTheme);
+  });
+});
 
 function applyTheme(themeCSS) {
   style.href = `stylesheets/${themeCSS}`;
 }
-
-option1.addEventListener("click", () => applyTheme(themes[0]));
-option2.addEventListener("click", () => applyTheme(themes[1]));
-option3.addEventListener("click", () => applyTheme(themes[2]));
